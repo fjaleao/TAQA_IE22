89f198d55e7fd946c2d5c821826a8918cbafbfa8
João Ribeiro <a16530@alunos.ipca.pt>
2022-07-12 10:27:55 +0100

    words frequency

--- hello.py
+++ hello.py
@@ -1,12 +1,26 @@
 import pandas as pd
 import numpy as np
+import speech_recognition as sr
+from io import StringIO
  
- 
-df = pd.DataFrame({'mytext':['I love Predictive Hacks!','How can I remove punctuations?'
-                             ,'He said: "This is cool!".']})
- 
+filename = "clip-pt"
+
+r = sr.Recognizer()
+
+AUDIO_FILE = f"{filename}.wav"
+
+with sr.AudioFile(AUDIO_FILE) as source:
+
+        audio = r.record(source)  # read the entire audio file                  
+
+        text = r.recognize_google(audio, language="pt-PT")
+        print(text)
 
 
+#StringData = StringIO(text)
+ 
+df = pd.DataFrame({'mytext': [text]})
+ 
  
 df["mytext_new"] = df['mytext'].str.lower().str.replace('[^\w\s]','')
 new_df = df.mytext_new.str.split(expand=True).stack().value_counts().reset_index()
