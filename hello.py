bb2be98bcdbd9a9ce06b6729f56deef2b5f14bf6
João Ribeiro <a16530@alunos.ipca.pt>
2022-07-12 10:10:15 +0100

    a

--- hello.py
+++ hello.py
@@ -1,2 +1,18 @@
-print("hello world")
-#isto e um comment lol
+import pandas as pd
+import numpy as np
+ 
+ 
+df = pd.DataFrame({'mytext':['I love Predictive Hacks!','How can I remove punctuations?'
+                             ,'He said: "This is cool!".']})
+ 
+
+
+ 
+df["mytext_new"] = df['mytext'].str.lower().str.replace('[^\w\s]','')
+new_df = df.mytext_new.str.split(expand=True).stack().value_counts().reset_index()
+ 
+new_df.columns = ['Word', 'Frequency'] 
+ 
+print(new_df)
+
+
