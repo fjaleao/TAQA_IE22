a7cb33e89a7da476ada03d4cd72baa83dc2928fe
fjaleao <fjaleao@gmail.com>
2022-07-14 19:22:05 +0100

    gui2

--- gui2.py
+++ gui2.py
@@ -1,10 +1,12 @@
 # Python program to create
 # a file explorer in Tkinter
+from asyncio.windows_events import NULL
 from asyncore import write
 from curses import window
 import os
 from platform import win32_edition
 from turtle import window_height
+from matplotlib.pyplot import text
 import pandas
 
 # import all components
@@ -16,6 +18,8 @@ from tkinter import ttk
 # import filedialog module
 from tkinter import filedialog
 from tkinter import messagebox
+
+from yaml import parse
 from analysis import analysis
 from keywords import keywords
 
@@ -40,9 +44,17 @@ def browseFiles():
                                                       "*.mp4*")))
 
     # Change label contents
-    label_file_explorer.configure(text=filename)
-
+    label_file_explorer.configure(text=filename, font=('Arial', 8))
     button_analysis.grid_remove()
+    button_explore.configure(text="Change File")
+
+    # if(label_file_explorer.get() != None):
+    #     print("devia estar a mostrar alguma cena")
+    #     showLanguageRow()
+    if(label_file_explorer.cget("text") != ""):
+        showLanguageRow()
+    else:
+        hideLanguageRow()
 
 #translate rgb to wtv format tkinter uses for colouring
 def fromRgb(rgb):
@@ -55,7 +67,7 @@ def Go():
         lang = "en-US"
     elif(combox.get() == "Portuguese"):
         lang = "pt-PT"
-    elif(combox.get() == "Madeirense"):
+    elif(combox.get() == "Madeirense"): # props ao hugo
         lang = "pt-PT"
 
     label_status.config(text="Status : Processing...")
@@ -96,10 +108,10 @@ def showAnalysis():
     L2 = Label(results, text="Rank")
     L3 = Label(results, text="Count")
 
-    print("AQUI")
+    # print("AQUI")
 
     for i in range(0,len(lista)):
-        print("ENTROU")
+        # print("ENTROU")
         Lt = Label(results, text=lista(i).texto)
         Lr = Label(results, text=lista(i).rank)
         Lc = Label(results, text=lista(i).count)
@@ -110,7 +122,32 @@ def showAnalysis():
     L2.grid(column=1,row=0)
     L3.grid(column=2,row=0)
     
-    btn_back = Button(results, text = "Back") 
+    btn_back = Button(results, text = "Back")
+
+def showLanguageRow():
+    label_lang.grid(column=1, row=5, sticky='W', **options)
+    combox.grid(column=1, row=5)
+    label_spacer3.forget()
+
+def hideLanguageRow():
+    label_spacer3.grid(column=1, row=5)
+    label_lang.forget()
+    combox.forget()
+
+def showTranscriptButton(event):
+    print("botaum")
+    # button_go.forget_grid()
+    button_go = Button(window,
+                    text="Transcript",
+                    font=('Arial, 30'),
+                    fg=fromRgb((182, 27, 35)),
+                    # width=40, height=4,
+                    command=Go,
+                    **button_options
+                    )
+    button_go.grid(column=1, row=7)
+
+#! def changeWindow():
 
 
 
@@ -118,7 +155,7 @@ def showAnalysis():
 window = Tk()
 
 # Set window title
-window.title('Great Title')
+window.title('Thematic Analysis of Qualitative Data')
 
 # Define window size
 window_width = 700
@@ -141,89 +178,124 @@ window.resizable(False, False)
 window.state('normal')
 
 # Load Critical Software logo
-logo_image = Image.open("cs_logo.jpg").resize((120,63))
+logo_image = Image.open("cs_logo.jpg") #!removed resize
 logo_photo = ImageTk.PhotoImage(logo_image)
 
 # Frame creation
 frame = Frame(window)
 
 # Define common options for form fields
-options = {'padx': 5, 'pady': 5} #!add more common options
+options = {'padx': 10, 'pady': 5} #!add more common options
+button_options = {'padx': 20, 'pady': 5}
 
-# Header Background label
-label_headBg = Label(window,
-                    background=fromRgb((182, 27, 35)),
-                    width=100, height=4)
-label_headBg.grid(column=1, row=1, **options)
+# Spacer labels
+def createSpacer():
+    return Label(window, height=2, background="white")
+
+label_spacer1 = createSpacer()
+label_spacer2 = createSpacer()
+label_spacer3 = createSpacer()
+label_spacer4 = createSpacer()
+label_spacer5 = createSpacer()
 
 # Header label
 label_header = Label(window,
                     image=logo_photo,
-                    # background=fromRgb((182, 27, 35)),
-                    
+                    background=fromRgb((182, 27, 35)),
+                    width=700, height=100
                     )
-label_header.grid(column=1, row=1, **options)
 
 # File Explorer label
 label_file_explorer = Label(window,
                             text="Select File",
-                            # width=40, height=4,
-                            fg="white",
+                            fg=fromRgb((182, 27, 35)),
                             font=('Arial', 15),
-                            # background=
+                            background="white"
                             )
-label_file_explorer.grid(column=1, row=1)
 
 # Language label
 label_lang = Label(window,
-                   text="Select Language",
-                   width=100, height=4,
-                   fg="red")
+                   text="Select Language :",
+                   fg=fromRgb((182, 27, 35)),
+                            font=('Arial', 15),
+                            background="white"
+                   )
+# label_lang.grid(column=1, row=4, sticky='W', **options)
 
+# 
 label_trs = Label(window)
 
+# Dummy Transcript label
+button_go = Label(window,
+                    text="Transcript",
+                    font=('Arial, 30'),
+                    fg="white",
+                    background="grey",
+                    **button_options
+                    )
+
 # Status label
-label_status = Label(window, text="Status:",
-                    width=100,
-                    height=4,
-                    fg="red")
+label_status = Label(window, text="Status :",
+                    font=('Arial', 15),
+                    fg=fromRgb((182, 27, 35)),
+                    background="white"
+                    )
                     
 
-vlist = ["English", "Portuguese", "Madeirense"]
+vlist = ["English", "Portuguese"]
 
-combox = ttk.Combobox(window, values=vlist)
-
-button_go = Button(window, text="Transcript", command=Go)
+combox = ttk.Combobox(window,
+                    values=vlist,
+                    font=('Arial', 15),
+                    state="readonly"
+                    )
+combox.bind('<<ComboboxSelected>>', showTranscriptButton)
 
 button_explore = Button(window,
                         text="Browse Files",
-                        command=browseFiles)
+                        fg=fromRgb((182, 27, 35)),
+                        command=browseFiles,
+                        **button_options
+                        )
 
-button_analysis = Button(
-    window, text="Analyse and Show Results", command=showAnalysis)
-
-button_exit = Button(window,
-                     text="Exit",
-                     command=exit)
+button_analysis = Button(window,
+                        text="Analyse and Show Results",
+                        command=showAnalysis,
+                        **button_options
+                        )
 
 
 # Grid method is chosen for placing
 # the widgets at respective positions
 # in a table like structure by
 # specifying rows and columns
+label_header.grid(column=1, row=1)
 
+label_spacer1.grid(column=1, row=2)
 
+button_explore.grid(column=1, row=3, sticky='W', **button_options)
+label_file_explorer.grid(column=1, row=3, **options)
 
-button_explore.grid(column=1, row=3)
+label_spacer2.grid(column=1, row=4)
 
-label_lang.grid(column=1, row=4)
-combox.grid(column=1, row=5)
+# Spacer that will turn into the language combobox
+# label_lang.grid(column=1, row=5, sticky='W', **options)
+# combox.grid(column=1, row=5)
+label_spacer3.grid(column=1, row=5)
 
-button_go.grid(column=1, row=6)
+label_spacer4.grid(column=1, row=6)
 
-label_status.grid(column=1, row=7)
+button_go.grid(column=1, row=7)
 
-button_exit.grid(column=1, row=8)
+label_spacer5.grid(column=1, row=8)
+
+label_status.grid(column=1, row=9)
+
+#todo
+
+# def init():
+    
+
 
 # Let the window wait for any events
 window.mainloop()
